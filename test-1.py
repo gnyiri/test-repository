12b7d8a94c6d6435b4f9b22c33a565c3f76c7af0
Gergely Nyiri <gergely.nyiri@eli-alps.hu>
2016-08-03 15:25:51 +0200

    Add tests

--- test-1.py
+++ test-1.py
@@ -18,10 +18,22 @@ class Test1(unittest.TestCase):
         pass
 
     def testTest1Tc1(self):
-        self.assertEqual(1, 0)
-        self.assertEqual(0, 1)
-        self.assertEqual(1, 1)
-        self.assertEqual(0, 0)
+        self.assertTrue(False)
+
+    def testTest1Tc2(self):
+        self.assertTrue(False)
+
+    def testTest1Tc3(self):
+        self.assertTrue(False)
+
+    def testTest1Tc4(self):
+        self.assertTrue(True)
+
+    def testTest1Tc5(self):
+        self.assertTrue(True)
+
+    def testTest1Tc6(self):
+        self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main()
